63efae673f49e09c00477b65b703829ce2a81410
Alison <alisongomezzv@hotmail.com>
2025-02-19 21:41:28 -0400

    supabase actualizacion

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iovagfbgagxouewjisak.supabase.co'; // Reemplaza con tu URL de Supabase
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvdmFnZmJnYWd4b3Vld2ppc2FrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzODg0MjYsImV4cCI6MjA1NDk2NDQyNn0.XKxGTNg4OpmGY7L1q6BzDhNBsJVA7Bhgv8gS1JOk-5Y'; // Reemplaza con tu clave pública
+const supabaseUrl = 'https://soxqtytndrewnrghvndw.supabase.co'; // Reemplaza con tu URL de Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNveHF0eXRuZHJld25yZ2h2bmR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwMTQ4ODYsImV4cCI6MjA1NTU5MDg4Nn0.z0bZLw4WfYSLoEuOta1QGg7QiDOvG1DdBfw6QMfdGZw'; // Reemplaza con tu clave pública
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
